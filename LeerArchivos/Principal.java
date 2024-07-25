bae5897b2f82499a24aa23c7a723970dd1c93f48
Salvador-GA <81882609+Salvador-GA@users.noreply.github.com>
2024-07-25 09:32:04 -0600

    Add files via upload

--- LeerArchivos/Principal.java
+++ LeerArchivos/Principal.java
@@ -14,8 +14,7 @@ import java.io.IOException;
 public class Principal {
     public static void main(String[] args) {
         // puede incluir la ruta relativa en su lugar, si el archivo miPrograma.uam se
-        // encuentra en
-        // la misma carpeta desde donde se esta ejecutadno el programa
+        // encuentra en la misma carpeta desde donde se esta ejecutadno el programa
         String archivo = "/home/salvador/Documentos/Compiladores/Compiladores/src/LeerArchivos/miPrograma.uam";
 
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
@@ -24,7 +23,7 @@ public class Principal {
             // colocamos una marca desde el inicio del buffer y que sera valida hasta 5000
             // caracteres adelante
             br.mark(5000);
-            
+
             // imprimir contenido caracter por caracter
             System.out.println("Contenido desdel archivo:\n");
             while ((caracter = br.read()) != -1) {
@@ -33,11 +32,12 @@ public class Principal {
 
             // regresamos a la marca establecida, al inicio del archivo en nuestro caso
             br.reset();
+            
             // imprimir contenido caracter por caracter convirtiendolo a mayusculas
             System.out.println("\n\nContenido desdel archivo en mayusculas:\n");
             while ((caracter = br.read()) != -1) {
                 if (Character.isAlphabetic(caracter)) {
-                    System.out.print(Character.toUpperCase((char)caracter));
+                    System.out.print(Character.toUpperCase((char) caracter));
                 } else {
                     System.out.print((char) caracter);
                 }
